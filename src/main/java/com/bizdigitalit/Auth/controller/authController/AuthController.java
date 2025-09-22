f2a7a9279c556eeb52ad23011019d6592bba246d
reemam-0223 <reema.m@bizdigitalit.com>
2025-09-22 21:00:59 +0530

    AuthController.java

--- src/main/java/com/bizdigitalit/Auth/controller/authController/AuthController.java
+++ src/main/java/com/bizdigitalit/Auth/controller/authController/AuthController.java
@@ -1,24 +1,26 @@
-package com.bizdigitalit.Auth.controller.authController;
+package com.bizdigitalit.Auth.controller;
 
 import com.bizdigitalit.Auth.dto.SignInRequest;
 import com.bizdigitalit.Auth.dto.SignupRequest;
+import com.bizdigitalit.Auth.dto.AuthResponse;
+import com.bizdigitalit.Auth.dto.RefreshTokenRequest;
 import com.bizdigitalit.Auth.service.UserService;
+import com.bizdigitalit.Auth.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @PostMapping("/signup")
@@ -27,7 +29,26 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<String> signIn(@RequestBody SignInRequest userSignInDto){
-        return ResponseEntity.ok().body(userService.signInUser(userSignInDto));
+    public ResponseEntity<AuthResponse> signIn(@RequestBody SignInRequest userSignInDto){
+        return ResponseEntity.ok(authService.signIn(userSignInDto));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
+        String refreshToken = request.getRefreshToken();
+
+        var user = authService.getUserByRefreshToken(refreshToken);
+
+        if (authService.isRefreshTokenValid(refreshToken, user)) {
+            String newAccessToken = authService.generateAccessToken(user);
+            return ResponseEntity.ok(
+                    AuthResponse.builder()
+                            .accessToken(newAccessToken)
+                            .refreshToken(refreshToken) // reuse or generate new one
+                            .build()
+            );
+        } else {
+            throw new RuntimeException("Refresh token expired, please login again");
+        }
     }
 }
